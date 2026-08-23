006218a580cbace6c726d502616a68b8f445320e
Rodrigo-dev7 <fcorodrigo.ti@gmail.com>
2026-08-23 18:41:24 -0300

    fix: reforca seguranca do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
@@ -7,13 +6,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    {window.location.pathname === "/" ? <Index /> : <NotFound />}
   </QueryClientProvider>
 );
 
